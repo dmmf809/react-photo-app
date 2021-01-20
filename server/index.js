8084ea4675deeac0cd55c6dd385d2e6d6cb362fd
dmmf809 <daeniellef1@gmail.com>
2021-01-19 20:45:41 -0700

    Server folder added

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "photoapp",
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+//Login with the right credentials
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+   db.query("SELECT * FROM users WHERE username = ? AND password = ?",
+   [username, password],
+   (err, result) => {
+     if(err) {
+        console.log(err);
+     } else {
+         res.send(result)
+     }
+   })
+})
+
+
+//Get all collections
+app.get("/collections", (req, res) => {
+  db.query("SELECT * FROM collection", (err, result) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//Get specific collection
+app.get("collections/:name", (req, res) => {
+  const name = req.params.name;
+
+  db.query(
+    "SELECT * FROM collections WHERE collection_name = ?",
+    [name],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
